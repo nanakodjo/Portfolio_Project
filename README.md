d74017e7960077669134afd1229dcf82f1ac2fd5
Nana Kodjo Acquah-Asare <nanakodjo@gmail.com>
2021-04-23 16:08:03 -0400

    Portfolio Project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio_Project
+#### On demand hair stylist and make-up artist.
+#### For people who are unable to go to salon
---
+++ main.js
@@ -0,0 +1,29 @@
+// let navbar = $(".navbar");
+
+// $(window).scroll(function(){
+//     let oTop = $(".section-2".offset().top-window.innerHeight);
+//     if($(window).scrollTop()> oTop){
+//         navbar.addClass("sticky");
+//     } else{
+//         navbar.removeClass("sticky")
+//     }
+// });
+
+// let alphaCount = function(selector){
+//     $(selector).each(function(){
+//         $(this).animate({
+//             Counter:$(this).text()
+//         },{
+//             duration:4000,
+//             easing: "swing",
+//             step: function(value){
+//                 $(this).text(Math.ceil(value));
+//             }
+//            } 
+//         );
+//     });
+// };
+
+
+            
+
